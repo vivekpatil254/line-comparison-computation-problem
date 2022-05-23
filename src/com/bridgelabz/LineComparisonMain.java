a1e3976d0082618654aaaff981715608649a394b
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 <Vivekpatil254@gmail.com>
2022-05-23 17:08:04 +0530

    UC3 commit

--- src/com/bridgelabz/LineComparisonMain.java
+++ src/com/bridgelabz/LineComparisonMain.java
@@ -26,17 +26,33 @@ public class LineComparisonMain {
         System.out.println("Enter the X and Y co-ordinates for second point of line 2 : ");
         x4 = scanner.nextInt();
         y4 = scanner.nextInt();
+            equals(x1, x2, x3, x4, y1, y2, y3, y4);
 
-        line1 = (int) Math.sqrt(((x2 - x1) ^ 2) * ((x2 - x1) ^ 2) + ((y2 - y1) ^ 2) * ((y2 - y1) ^ 2));
+            compareTo(x1, x2, x3, x4, y1, y2, y3, y4);
 
-        line2 = (int) Math.sqrt(((x4 - x3) ^ 2 * ((x4 - x3) ^ 2) + ((y4 - y3) ^ 2) * ((y4 - y3) ^ 2)));
     }
-    public void equals(int x1, int x2, int x3, int x4, int y1, int y2, int y3, int y4) {
+    public static void equals(int x1, int x2, int x3, int x4, int y1, int y2, int y3, int y4) {
 
         int line1 = (int) Math.sqrt(((x2 - x1) ^ 2) * ((x2 - x1) ^ 2) + ((y2 - y1) ^ 2) * ((y2 - y1) ^ 2));
         int line2 = (int) Math.sqrt(((x4 - x3) ^ 2 * ((x4 - x3) ^ 2) + ((y4 - y3) ^ 2) * ((y4 - y3) ^ 2)));
         if (line1==line2)
             System.out.println("the line 1 and line 2 both are equal");
+        else
+            System.out.println("both line are not equal");
+    }
+
+    public static void compareTo(int x1, int x2, int x3, int x4, int y1, int y2, int y3, int y4){
+
+        int line1 = (int) Math.sqrt(((x2 - x1) ^ 2) * ((x2 - x1) ^ 2) + ((y2 - y1) ^ 2) * ((y2 - y1) ^ 2));
+        int line2 = (int) Math.sqrt(((x4 - x3) ^ 2 * ((x4 - x3) ^ 2) + ((y4 - y3) ^ 2) * ((y4 - y3) ^ 2)));
+
+        if (line1==line2)
+            System.out.println("the line 1 and line 2 both are equal");
+        else if(line1>line2)
+            System.out.println("the line 1 is greater than line 2");
+        else
+            System.out.println("the line 1 is less than line 2");
+
     }
 
 }
